5e6e693e305af7c1362f1d754090fa5bb2c11fc8
stmarple <stmarple@hotmail.com>
2020-06-15 19:42:38 -0400

    Add files via upload

--- js/jsonajax.js
+++ js/jsonajax.js
@@ -1,51 +1,45 @@
 
 
-(
-	function()
-	{
-		var httpRequest;
-		document.getElementById("btn").onclick = function() 
-		{
-		makeRequest("https://github.com/stmarple/stmarple.github.io/schools.json");
-		};
 
-		function makeRequest(url) {
-			httpRequest = new XMLHttpRequest();  // Create XMLHttpRequest object
-
-			if (!httpRequest) {
-				alert('Exiting : Cannot create an XMLHTTP Instance');
-				return false;
-			}
-			httpRequest.onreadystatechange = alertContents;
-			alert(url);
-			httpRequest.open('GET', url, true);
-			httpRequest.send(null);
-		};
-		function alertContents(){
-			alert()
-			if (httpRequest.status === 200){
-				alert(httpRequest.responseText);
-				responseObject = JSON.parse(httpRequest.responseText);
-
-				var newContent = '';
-				for (var i = 0; i < responseObject.degrees.length; i++) {
-					newContent =   `<div class="event">;
-									<p> responseObject.degrees[i].name </p>
-									</div>
-									`
-					}
-			}
-			else{
-				alert('There was a problem with the request.');
-			}
-			document.getElementById('content').innerHTML = newContent;
-		}
-	}
-)();
-
-
+var httpRequest;
 document.getElementById('btn').addEventListener("click", button_click_event)
 function button_click_event(){
-	document.getElementById('demo').innerHTML = ""
+	makeRequest("https://github.com/stmarple/stmarple.github.io/schools.json");
+};
+
+function makeRequest(url) {
+	httpRequest = new XMLHttpRequest();  // Create XMLHttpRequest object
+
+	if (!httpRequest) {
+		alert('Exiting : Cannot create an XMLHTTP Instance');
+		return false;
+	}
+	httpRequest.onreadystatechange = alertContents;
+	httpRequest.open('GET', url, true);
+	httpRequest.send();
+};
+function alertContents(){
+	if(httpRequest.readyState == XMLHttpRequest.DONE) { 
+
+		alert(httpRequest.status)
+
+		if (httpRequest.status === 200){
+			alert(httpRequest.responseText);
+
+			responseObject = JSON.parse(httpRequest.responseText);
+
+			var newContent = '';
+			for (var i = 0; i < responseObject.degrees.length; i++) {
+				newContent +=   `<div class="event">;
+								<p> responseObject.degrees[i].name </p>
+								</div>
+								`
+				}
+		}
+		else{
+			alert('There was a problem with the request.');
+		}
+		document.getElementById('content').innerHTML = newContent;
+	}
 }
 
