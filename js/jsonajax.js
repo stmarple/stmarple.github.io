b4bee7487ebdb3c41a4d3aac2488eb728f72df69
stmarple <stmarple@hotmail.com>
2020-06-16 19:22:24 -0400

    Add files via upload

--- js/jsonajax.js
+++ js/jsonajax.js
@@ -2,11 +2,14 @@
 
 
 var httpRequest;
+
+// event handler for button, 'btn'
 document.getElementById('btn').addEventListener("click", button_click_event)
 function button_click_event(){
 	makeRequest("https://stmarple.github.io/schools.json");
 };
 
+// Code to create a request to get JSON information on github accout
 function makeRequest(url) {
 	httpRequest = new XMLHttpRequest();  // Create XMLHttpRequest object
 
@@ -20,33 +23,34 @@ function makeRequest(url) {
 };
 function alertContents(){
 	if(httpRequest.readyState == XMLHttpRequest.DONE) { 
-
-		alert(httpRequest.status)
-
 		if (httpRequest.status === 200){
-			alert(httpRequest.responseText);
+			//alert(httpRequest.responseText);                       // alert the JSON text
 
-			responseObject = JSON.parse(httpRequest.responseText);
+			responseObject = JSON.parse(httpRequest.responseText); 
 
-			var attrlist = ['name', 'major','degree type','year completed']
+			// Create resulting table
+			// attribute list includes all column headers, which are attributes in JSON file
+
+			var attrlist = ['name', 'major','degree type','year completed'] 
 			var tbl = document.getElementById('tbl');
-			var th = '';
+			var th = '';                               // initialize table header
 
+			// Create table headers by looping through the attribute list
 			for (var i = 0; i < attrlist.length; i++) {
-				th += '<th>' + attrlist[i] + '</th>'
+				th += '<th>' + attrlist[i] + '</th>'  
 			}
 
+			// Create table rows
 			var tr = document.createElement("tr");
 			tr.innerHTML = th;
 			tbl.appendChild(tr);
 
-			var newContent = '';
+			var newContent = '';  // initialize variable
 			for (var i = 0; i < responseObject.degrees.length; i++) {
-			//	newContent +=   '<div class="event"><p>' + responseObject.degrees[i].name + '</p></div>'
-				
-				var tr = document.createElement("tr");
-				var td = '';
+				var tr = document.createElement("tr");  // reassign var tr for every loop pass
+				var td = '';                            // (re)initialize table data variable
 
+				// for each degree, add table data for all of its attributes
 				for (var j = 0; j < attrlist.length; j++) {
 					td += '<td>' + responseObject.degrees[i][attrlist[j]] + '</td>'
 				}
