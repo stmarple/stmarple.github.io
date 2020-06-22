42bf88595370409d3df6c899054a655f874f64cf
stmarple <stmarple@hotmail.com>
2020-06-22 14:34:20 -0400

    Add files via upload

--- project/js/main.js
+++ project/js/main.js
@@ -109,65 +109,40 @@ var app = new Vue(	{
 
 
 // AJAX
-var httpRequest;
+function fetchdata(datasrc) {
 
-// event handler for button, 'btn'
-document.getElementById('btn_school').addEventListener("click", button_click_event)
-function button_click_event(){
-	makeRequest("https://stmarple.github.io/project/skills.json");
-};
-
-// Code to create a request to get JSON information on github accout
-function makeRequest(url) {
-	httpRequest = new XMLHttpRequest();  // Create XMLHttpRequest object
-
-	if (!httpRequest) {
-		alert('Exiting : Cannot create an XMLHTTP Instance');
-		return false;
-	}
-	httpRequest.onreadystatechange = alertContents;
-	httpRequest.open('GET', url, true);
-	httpRequest.send();
-};
-function alertContents(){
-	if(httpRequest.readyState == XMLHttpRequest.DONE) { 
-		if (httpRequest.status === 200){
-
-			responseObject = JSON.parse(httpRequest.responseText); 
-
-			// Create resulting table
-			// attribute list includes all column headers, which are attributes in JSON file
-
-			var attrlist = ['location', 'skill'] 
-			var tbl = document.getElementById('tbl_school');
-			var th = '';                               // initialize table header
-
-			// Create table headers by looping through the attribute list
-			for (var i = 0; i < attrlist.length; i++) {
-				th += '<th>' + attrlist[i] + '</th>'  
-			}
-
-			// Create table rows
-			var tr = document.createElement("tr");
-			tr.innerHTML = th;
-			tbl.appendChild(tr);
-
-			var newContent = '';                                         // initialize variable
-			for (var i = 0; i < responseObject.degrees.length; i++) {
-				var tr = document.createElement("tr");                  // reassign var tr for every loop pass
-				var td = '';                                           // (re)initialize table data variable
-
-				// for each degree, add table data for all of its attributes
-				for (var j = 0; j < attrlist.length; j++) {
-					td += '<td>' + responseObject.degrees[i][attrlist[j]] + '</td>'
-				}
-				tr.innerHTML = td;
-				tbl.appendChild(tr);
-			}
-		}
-		else{
-			alert('There was a problem with the request.');
-		}
-		document.getElementById('content').innerHTML = newContent;
-	}
+    /* put your xmlhttprequest code here */
+    return json_object;
 }
+
+
+function populate_div(leftright) {
+  var data = {};
+  var header = [];
+
+  switch(leftright) {
+      case "table-left":
+          data = fetchdata("https://smarple.github.io/skills.json")['school'];
+          header = [..];
+      case "table-right":
+           data = fetchdata("https://smarple.github.io/skills.json")['job'];
+           header = […];
+  }
+     
+  /* construct HTML/DOM objects here */   
+  
+  //var frag =document.createDocumentFragment(); // use fragment to avoid redraw
+  var ul = document.createElement('ul');
+
+  /* loop through the dataset */
+  data.map(function iter_data(data_e) { 
+    
+    /* remember data_e is a value, scaler   */
+    var li = document.createElement('li').innerText(data_e)
+    ul.appendChild(li);
+
+  });
+  //frag.appendChild('ul')
+  document.getElementById(leftright).appendChild(ul);
+}
+
